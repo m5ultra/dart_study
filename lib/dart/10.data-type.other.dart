92400e28e8aac98e4bf2e97f358271b3cb79c7a1
Dendi <352671309@qq.com>
2022-10-03 18:46:41 +0800

    Feat: 类型

---
+++ lib/dart/10.data-type.other.dart
@@ -0,0 +1,23 @@
+void main() {
+  String str = '😀';
+  print(str);
+  print(str.length); // UTF-16
+  print(str.runes.length); // UTF-32 表示的字符
+
+  // Runes 可以将 UTF-32 字符集表示的内容转成符号
+  Runes input = Runes('\u{1f680}');
+  // print(String.fromCharCode(input));
+
+  // Symbol 声明
+
+  Symbol s = #abc;
+  print(s);
+  Symbol b = const Symbol('abc');
+  print(b);
+  print(#abc == const Symbol('abc'));
+
+  dynamic foo = 'boo';
+  print(foo);
+  foo = 3;
+  print(foo);
+}
