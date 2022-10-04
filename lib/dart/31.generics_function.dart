683059f1f087dd135e776aa1080c751bb481dc4a
Dendi <352671309@qq.com>
2022-10-05 00:20:44 +0800

    Feat: 泛型函数

---
+++ lib/dart/31.generics_function.dart
@@ -0,0 +1,14 @@
+String getData(String v) {
+  return v;
+}
+
+T getData02<T>(T v) {
+  return v;
+}
+
+void main() {
+  print(getData('getData'));
+  // print(getData(1)); // The argument type 'int' can't be assigned to the parameter type 'String'
+  print(getData02<String>('get data 2'));
+  print(getData02<int>(1));
+}
