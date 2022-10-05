ad660482a0a8bbb682d85face43d85c0cd6f53f2
Dendi <352671309@qq.com>
2022-10-05 12:16:00 +0800

    Feat: 引入冲突解决

---
+++ lib/dart/39.lib_alias.dart
@@ -0,0 +1,7 @@
+import 'lib/custom.dart';
+import 'lib/functions.dart' as functions; // 引入作为 functions
+
+void main () {
+f1();
+functions.f1(); // 调用functions.f1();
+}
---
+++ lib/dart/lib/functions.dart
@@ -0,0 +1,9 @@
+library functions;
+
+void f1() {
+  print('functions is running');
+}
+
+void hello() {
+  print('Hello World');
+}
